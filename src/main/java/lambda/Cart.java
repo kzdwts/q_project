ef94c3783cf9a39aa24f06535b7f6502961fb92c
kangyong <kangyong_wts@sina.com>
2020-09-16 19:18:39 +0800

    lambda表达式联系 1、遍历map 2、map转list 3、list转map

---
+++ src/main/java/lambda/Cart.java
@@ -0,0 +1,44 @@
+package lambda;
+
+import lombok.Data;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Description: 汽车类
+ * @author: kangyong
+ * @date: 2020/9/16 18:49
+ * @version: v1.0
+ */
+@Data
+public class Cart {
+
+    private Integer id;
+
+    /**
+     * 品牌
+     */
+    private String brand;
+
+    /**
+     * 重量
+     */
+    private Float weight;
+
+    /**
+     * 颜色
+     */
+    private String color;
+
+    public Cart(Integer id, String brand) {
+        this.id = id;
+        this.brand = brand;
+    }
+
+    public Cart(Integer id, String brand, Float weight, String color) {
+        this.id = id;
+        this.brand = brand;
+        this.weight = weight;
+        this.color = color;
+    }
+}
