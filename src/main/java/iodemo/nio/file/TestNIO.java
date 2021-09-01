976593ef371b8e9018e2b6bbd8f2d5bb377f62cf
kangyong <kangyong_wts@sina.com>
2021-09-01 22:41:15 +0800

    nio写文件

---
+++ src/main/java/iodemo/nio/file/TestNIO.java
@@ -0,0 +1,37 @@
+package iodemo.nio.file;
+
+import org.junit.Test;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Description: 通过nio实现io流
+ * @author: Kang Yong
+ * @date: 2021/9/1 22:27
+ * @version: v1.0
+ */
+public class TestNIO {
+
+    @Test
+    public void test01() throws IOException {
+        // 创建输出流
+        FileOutputStream fos = new FileOutputStream("wanglili.txt");
+        // 获取通道
+        FileChannel channel = fos.getChannel();
+        // 缓冲区
+        ByteBuffer buffer = ByteBuffer.allocate(1024);
+        // 存数据
+        buffer.put("Hello NIO".getBytes());
+        buffer.flip();
+        // 写出
+        channel.write(buffer);
+        fos.close();
+    }
+
+}
