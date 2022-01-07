6f5e53013d2685f0c876bf802e2fdad14e6122c9
kangyong <kangyong_wts@sina.com>
2022-01-07 11:20:35 +0800

    feat:有人相爱，有人开车看海，有人LeetCode第一题都做不出来

---
+++ src/main/java/top/kangyong/study02/leetcode/LeetCodeStudy01.java
@@ -0,0 +1,35 @@
+package top.kangyong.study02.leetcode;
+
+import org.junit.Test;
+
+/**
+ * leetcode 学习
+ *
+ * @author Kang Yong
+ * @date 2022/1/7
+ * @see 'https://leetcode-cn.com/problems'
+ * @since 1.0.0
+ */
+public class LeetCodeStudy01 {
+
+    /**
+     * 给定一个整数数组 nums 和一个整数目标值 target，
+     * 请你在该数组中找出 和为目标值 target  的那 两个 整数，
+     * 并返回它们的数组下标。
+     */
+    @Test
+    public void twoSum() {
+        int[] nums = {11, 7, 11, 2};
+        int target = 9;
+
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    System.out.println("坐标为：[" + i + ", " + j + "]");
+                    return;
+                }
+            }
+        }
+
+    }
+}
