5f617182b474acd19f6f0abc35164baa79da6078
kangyong <kangyong_wts@sina.com>
2022-01-17 15:23:07 +0800

    feat: GMT时间

--- src/main/java/top/kangyong/study02/datecode/DateTypeTest.java
+++ src/main/java/top/kangyong/study02/datecode/DateTypeTest.java
@@ -4,6 +4,9 @@ import org.junit.Test;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.Locale;
@@ -57,6 +60,17 @@ public class DateTypeTest {
         SimpleDateFormat sdf2 = new SimpleDateFormat("EEE, d MMM yyyy HH:mm:ss 'GMT'", Locale.US);
         sdf2.setTimeZone(TimeZone.getTimeZone("GMT"));
         System.out.println(sdf2.format(new Date()));
+    }
+
+    /**
+     * 显示GMTdate
+     */
+    @Test
+    public void showGMTDate2() {
+        String gmtDate = LocalDateTime.now().atZone(ZoneId.of("GMT")).format(DateTimeFormatter.RFC_1123_DATE_TIME);
+        System.out.println(gmtDate);
+//        Mon, 17 Jan 2022 10:22:26 GMT
+//        Mon, 17 Jan 2022 02:22:48 GMT
     }
 
 }
