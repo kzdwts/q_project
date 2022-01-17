e318e0ad8d3639f60c58e7b71e7aa739e4df4659
kangyong <kangyong_wts@sina.com>
2022-01-17 20:58:34 +0800

    时间计算处理

---
+++ src/main/java/top/kangyong/study02/datecode/DateCalcTest.java
@@ -0,0 +1,36 @@
+package top.kangyong.study02.datecode;
+
+import org.apache.commons.lang3.time.DateUtils;
+import org.junit.Test;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * 时间计算
+ *
+ * @author Kang Yong
+ * @date 2022/1/17
+ * @since 1.0.0
+ */
+public class DateCalcTest {
+
+    /**
+     * 时间相加
+     *
+     * @author Kang Yong
+     * @date 2022/1/17
+     */
+    @Test
+    public void addDateDayTest() {
+        // 自然天
+        Date date = DateUtils.addDays(new Date(), 1);
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd 00:00:00");
+        System.out.println(sdf.format(date));
+
+        // 24小时制度
+        SimpleDateFormat sdf2 = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        System.out.println(sdf2.format(date));
+    }
+
+}
