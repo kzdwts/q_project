3ee714e0cf0316934b76a0a38d1ea17aed304325
kangyong <kangyong_wts@sina.com>
2022-02-23 10:22:36 +0800

    feat: list ﬁndAny用于获取流中随机的某一个元素，并且利用短路在找到结果时，立即结束

--- src/main/java/top/kangyong/study02/listcode/ListTest.java
+++ src/main/java/top/kangyong/study02/listcode/ListTest.java
@@ -5,7 +5,10 @@ import top.kangyong.study02.model.Person;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+import java.util.Set;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * java8操作list
@@ -129,6 +132,38 @@ public class ListTest {
             System.out.println("集合*所有*学生的年龄都大于18");
         } else {
             System.out.println("集合有年龄都小于18的学生");
+        }
+    }
+
+    /**
+     * ﬁndAny用于获取流中随机的某一个元素，并且利用短路在找到结果时，立即结束
+     *
+     * @author Kang Yong
+     * @date 2022/2/23
+     */
+    @Test
+    public void test06() {
+        List<Person> personList = new ArrayList<>(6);
+        personList.add(new Person(1, 0, 18, "王丽丽", "中国"));
+        personList.add(new Person(2, 1, 19, "储小姣", "美国"));
+        personList.add(new Person(5, 0, 16, "钱梦婷", "中国"));
+        personList.add(new Person(4, 1, 17, "徐慧文", "日本"));
+        personList.add(new Person(3, 0, 20, "张丹妮", "中国"));
+        personList.add(new Person(3, 0, 20, "张丹妮", "中国"));
+
+        // 找出性别为1的学生
+        Stream<Person> personStream = personList.stream().filter(bean -> bean.getGender().intValue() == 1);
+        Set<Person> personSet = personStream.collect(Collectors.toSet());
+        for (Person person : personSet) {
+            System.out.println(person.toString());
+        }
+
+        // ﬁndAny用于获取流中随机的某一个元素，并且利用短路在找到结果时，立即结束
+        Optional<Person> personOptional = personList.stream().filter(bean -> bean.getGender().intValue() == 1).findAny();
+        if (personOptional.isPresent()) {
+            Person person = personOptional.get();
+            System.out.println("===找出一个");
+            System.out.println(person);
         }
     }
 }
