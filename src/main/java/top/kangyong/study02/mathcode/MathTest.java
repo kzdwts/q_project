71a2e8f9b5dc12335207f6c30c346e402e40c63c
kangyong <kangyong_wts@sina.com>
2022-05-12 11:10:41 +0800

    feat: Integer compareTo test

--- src/main/java/top/kangyong/study02/mathcode/MathTest.java
+++ src/main/java/top/kangyong/study02/mathcode/MathTest.java
@@ -20,10 +20,29 @@ public class MathTest {
         BigDecimal b3 = new BigDecimal(200.00);
         BigDecimal b4 = new BigDecimal(0.00);
 
+        System.out.println(b1.compareTo(BigDecimal.ZERO) > 0);
+        System.out.println(b2.compareTo(BigDecimal.ZERO) > 0);
+        System.out.println(b3.compareTo(BigDecimal.ZERO) > 0);
+        System.out.println(b4.compareTo(BigDecimal.ZERO) <= 0);
 
-        System.out.println(b1.compareTo(BigDecimal.ZERO) > 0);;
-        System.out.println(b2.compareTo(BigDecimal.ZERO) > 0);;
-        System.out.println(b3.compareTo(BigDecimal.ZERO) > 0);;
-        System.out.println(b4.compareTo(BigDecimal.ZERO) <= 0);;
+    }
+
+    /**
+     * Integer 比较大小
+     *
+     * @author Kang Yong
+     * @date 2022/5/12
+     */
+    @Test
+    public void integerCompareToTest01() {
+        Integer a = 78901;
+        Integer b = 7890;
+        System.out.println(a.compareTo(b));
+        System.out.println(b.compareTo(a));
+        // 结论：a.compareTo(b)
+        // a > b → 结果是 1
+        // a < b → 结果是 -1
+        // a == b → 结果是0
+
     }
 }
