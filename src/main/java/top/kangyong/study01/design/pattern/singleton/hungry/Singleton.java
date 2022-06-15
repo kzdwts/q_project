ec63a0a0d25abc1369a7e7d0f397900731c61dff
kangyong <kangyong_wts@sina.com>
2022-06-15 09:20:09 +0800

    feat: 单例模式：饿汉式

---
+++ src/main/java/top/kangyong/study01/design/pattern/singleton/hungry/Singleton.java
@@ -0,0 +1,25 @@
+package top.kangyong.study01.design.pattern.singleton.hungry;
+
+/**
+ * 饿汉式
+ * <p>
+ * 单例模式
+ *
+ * @author Kang Yong
+ * @date 2022/6/15
+ * @since 1.0.0
+ */
+public class Singleton {
+
+    /**
+     * 直接创建
+     */
+    private static Singleton instance = new Singleton();
+
+    private Singleton() {
+    }
+
+    public static Singleton getInstance() {
+        return instance;
+    }
+}
