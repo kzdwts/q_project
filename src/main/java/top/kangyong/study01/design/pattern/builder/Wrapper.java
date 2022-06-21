3d133e7207c7548945eb4837e8840e138e263989
kangyong <kangyong_wts@sina.com>
2022-06-21 09:11:40 +0800

    feat: 建造者模式Builder

---
+++ src/main/java/top/kangyong/study01/design/pattern/builder/Wrapper.java
@@ -0,0 +1,17 @@
+package top.kangyong.study01.design.pattern.builder;
+
+/**
+ * 创建实现 Packing 接口的实体类
+ * <p>
+ * 建造者模式
+ *
+ * @author Kang Yong
+ * @date 2022/6/21
+ * @since 1.0.0
+ */
+public class Wrapper implements Packing {
+    @Override
+    public String pack() {
+        return "Wrapper";
+    }
+}
