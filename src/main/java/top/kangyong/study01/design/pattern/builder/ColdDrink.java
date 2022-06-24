c3aedb2d90538c1f792d9f125684d8d468bf2cc5
kangyong <kangyong_wts@sina.com>
2022-06-24 09:27:25 +0800

    feat: 建造者模式Builder,冷饮用瓶子装

---
+++ src/main/java/top/kangyong/study01/design/pattern/builder/ColdDrink.java
@@ -0,0 +1,21 @@
+package top.kangyong.study01.design.pattern.builder;
+
+/**
+ * 冷饮
+ * <p>
+ * 建造者模式
+ *
+ * @author Kang Yong
+ * @date 2022/6/24
+ * @since 1.0.0
+ */
+public abstract class ColdDrink implements Item {
+
+    @Override
+    public Packing packing() {
+        return new Bottle();
+    }
+
+    @Override
+    public abstract float price();
+}
