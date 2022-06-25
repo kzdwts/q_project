640cba02f3b55c18642f4ed1361df12838e7117e
kangyong <kangyong_wts@sina.com>
2022-06-25 22:53:33 +0800

    feat: 原型模式

---
+++ src/main/java/top/kangyong/study01/design/pattern/prototype/Shape.java
@@ -0,0 +1,38 @@
+package top.kangyong.study01.design.pattern.prototype;
+
+/**
+ * 原型模式
+ *
+ * @author Kang Yong
+ * @date 2022/6/25
+ * @since 1.0.0
+ */
+public abstract class Shape implements Cloneable {
+
+    private String id;
+    protected String type;
+
+    abstract void draw();
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public Object clone() {
+        Object clone = null;
+        try {
+            clone = super.clone();
+        } catch (CloneNotSupportedException e) {
+            e.printStackTrace();
+        }
+        return clone;
+    }
+}
