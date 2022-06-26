54dc7d0eac7bb5e58b3301ed318be1a64a7934be
kangyong <kangyong_wts@sina.com>
2022-06-26 18:04:57 +0800

    feat: 原型模式

---
+++ src/main/java/top/kangyong/study01/design/pattern/prototype/Rectangle.java
@@ -0,0 +1,21 @@
+package top.kangyong.study01.design.pattern.prototype;
+
+
+/**
+ * 原型模式
+ *
+ * @author Kang Yong
+ * @date 2022/6/26
+ * @since 1.0.0
+ */
+public class Rectangle extends Shape {
+
+    public Rectangle() {
+        type = "Rectangle";
+    }
+
+    @Override
+    public void draw() {
+        System.out.println("Inside rectangle drow() method");
+    }
+}
