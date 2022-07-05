17ebcdf4eb962436f974083ac1eac8ec6ff6a76d
kangyong <kangyong_wts@sina.com>
2022-07-05 09:03:15 +0800

    feat: adapter

---
+++ src/main/java/top/kangyong/study01/design/pattern/adapter/MediaAdapter.java
@@ -0,0 +1,24 @@
+package top.kangyong.study01.design.pattern.adapter;
+
+/**
+ * 媒体适配器
+ *
+ * @author Kang Yong
+ * @date 2022/7/5
+ * @since 1.0.0
+ */
+public class MediaAdapter implements MediaPlayer {
+
+    /**
+     * 播放
+     *
+     * @param audioType {@link String}
+     * @param fileName  {@link String}
+     * @author Kang Yong
+     * @date 2022/7/5
+     */
+    @Override
+    public void play(String audioType, String fileName) {
+
+    }
+}
