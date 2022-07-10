937c6fdde7cae8136b8771ef8e1eb65813e2a66a
kangyong <kangyong_wts@sina.com>
2022-07-10 23:16:17 +0800

    feat: 适配器模式

--- src/main/java/top/kangyong/study01/design/pattern/adapter/AdapterPatternDemo.java
+++ src/main/java/top/kangyong/study01/design/pattern/adapter/AdapterPatternDemo.java
@@ -12,5 +12,7 @@ public class AdapterPatternDemo {
     public static void main(String[] args) {
         AudioPlayer audioPlayer = new AudioPlayer();
 
+        audioPlayer.play("mp3", "beyond the horizon.mp3");
+
     }
 }
