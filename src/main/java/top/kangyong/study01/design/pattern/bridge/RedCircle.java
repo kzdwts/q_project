453b345285b3c2e5b0f9dc9c7c9f5342835ffb09
kangyong <kangyong_wts@sina.com>
2022-07-18 22:08:18 +0800

    feat: 桥接模式

--- src/main/java/top/kangyong/study01/design/pattern/bridge/RedCircle.java
+++ src/main/java/top/kangyong/study01/design/pattern/bridge/RedCircle.java
@@ -1,15 +1,16 @@
 package top.kangyong.study01.design.pattern.bridge;
 
 /**
- * TODO description.
+ * 红色的园
  *
  * @author Kang Yong
  * @date 2022/7/14
  * @since 1.0.0
  */
-public class RedCircle implements DrawAPI{
+public class RedCircle implements DrawAPI {
+
     @Override
     public void drawCircle(int radius, int x, int y) {
-
+        System.out.println("Drawing Circle[ color: red, radius: " + radius + ", x:" + x + ", y " + y + "]");
     }
 }
