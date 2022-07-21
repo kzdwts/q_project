2898629af5f945869475d7cf0f73fdeadb6e7710
kangyong <kangyong_wts@sina.com>
2022-07-21 23:25:26 +0800

    feat: 接口隔离原则

---
+++ src/main/java/top/kangyong/study01/design/pattern/bridge/GreenCircle.java
@@ -0,0 +1,17 @@
+package top.kangyong.study01.design.pattern.bridge;
+
+/**
+ * green circle
+ * <p>
+ * 桥接模式
+ *
+ * @author Kang Yong
+ * @date 2022/7/21
+ * @since 1.0.0
+ */
+public class GreenCircle implements DrawAPI {
+    @Override
+    public void drawCircle(int radius, int x, int y) {
+
+    }
+}
