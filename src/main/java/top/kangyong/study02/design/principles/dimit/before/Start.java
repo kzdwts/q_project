50836ae5b4d8279404f845882e366a25f4291fac
kangyong <kangyong_wts@sina.com>
2022-07-23 23:31:48 +0800

    feat: 接口隔离原则

---
+++ src/main/java/top/kangyong/study02/design/principles/dimit/before/Start.java
@@ -0,0 +1,13 @@
+package top.kangyong.study02.design.principles.dimit.before;
+
+/**
+ * 明星
+ * <p>
+ * 迪米特法则
+ *
+ * @author Kang Yong
+ * @date 2022/7/23
+ * @since 1.0.0
+ */
+public class Start {
+}
