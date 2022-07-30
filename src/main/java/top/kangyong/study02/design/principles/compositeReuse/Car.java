23ed0eef0e106616f85ee0b14bdf8da5d73ea9d6
kangyong <kangyong_wts@sina.com>
2022-07-30 23:58:43 +0800

    feat: 合成复用原则

---
+++ src/main/java/top/kangyong/study02/design/principles/compositeReuse/Car.java
@@ -0,0 +1,13 @@
+package top.kangyong.study02.design.principles.compositeReuse;
+
+/**
+ * 汽车类
+ * <p>
+ * 合成复用原则
+ *
+ * @author Kang Yong
+ * @date 2022/7/30
+ * @since 1.0.0
+ */
+public interface Car {
+}
