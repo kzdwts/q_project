753742fd109e1784cc7e1c9db920c66420e142f7
kangyong <kangyong_wts@sina.com>
2022-08-01 14:21:23 +0800

    feat: 过滤器模式

---
+++ src/main/java/top/kangyong/study01/design/pattern/filter/OrCriteria.java
@@ -0,0 +1,34 @@
+package top.kangyong.study01.design.pattern.filter;
+
+import java.util.List;
+
+/**
+ * 过滤器模式
+ *
+ * @author Kang Yong
+ * @date 2022/8/1
+ * @since 1.0.0
+ */
+public class OrCriteria implements Criteria {
+
+    private Criteria criteria;
+    private Criteria otherCriteria;
+
+    public OrCriteria(Criteria criteria, Criteria otherCriteria) {
+        this.criteria = criteria;
+        this.otherCriteria = otherCriteria;
+    }
+
+    @Override
+    public List<Person> meetCriteria(List<Person> persons) {
+        List<Person> firstCriteriaItems = criteria.meetCriteria(persons);
+        List<Person> otherCriteriaItems = otherCriteria.meetCriteria(persons);
+
+        for (Person person : persons) {
+            if (!firstCriteriaItems.contains(person)) {
+                firstCriteriaItems.add(person);
+            }
+        }
+        return firstCriteriaItems;
+    }
+}
