cfd1c8fbe978d20ed10697e0ccc461149a13ced1
kangyong <kangyong_wts@sina.com>
2022-08-03 23:00:30 +0800

    feat: 单例模式 饿汉式 静态成员变量

--- src/main/java/top/kangyong/study02/design/pattern/creatormode/singleton/demo1/Singleton.java
+++ src/main/java/top/kangyong/study02/design/pattern/creatormode/singleton/demo1/Singleton.java
@@ -10,4 +10,28 @@ package top.kangyong.study02.design.pattern.creatormode.singleton.demo1;
  * @since 1.0.0
  */
 public class Singleton {
+
+    /**
+     * 构造一个私有方法
+     *
+     * @author Kang Yong
+     * @date 2022/8/3
+     */
+    private Singleton() {
+
+    }
+
+    // 在本类中创建一个本类对象
+    private static Singleton instance = new Singleton();
+
+    /**
+     * 提供一个公共的访问方式，让外界可以获取该对象
+     *
+     * @return {@link Singleton}
+     * @author Kang Yong
+     * @date 2022/8/3
+     */
+    public static Singleton getInstance() {
+        return instance;
+    }
 }
