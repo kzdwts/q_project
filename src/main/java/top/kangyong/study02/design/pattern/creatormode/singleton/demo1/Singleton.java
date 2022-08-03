74749a711abd339dacf546ceb65cfad16874ebaf
kangyong <kangyong_wts@sina.com>
2022-08-03 22:54:15 +0800

    feat: 单例模式 饿汉式 静态成员变量

---
+++ src/main/java/top/kangyong/study02/design/pattern/creatormode/singleton/demo1/Singleton.java
@@ -0,0 +1,13 @@
+package top.kangyong.study02.design.pattern.creatormode.singleton.demo1;
+
+/**
+ * 单例模式：饿汉式 静态成员变量
+ * <p>
+ * 创建型模式
+ *
+ * @author Kang Yong
+ * @date 2022/8/3
+ * @since 1.0.0
+ */
+public class Singleton {
+}
