dceb635a9360f11c6600f72e72ecfce6755d6665
kangyong <kangyong_wts@sina.com>
2022-08-06 17:14:58 +0800

    feat: 过滤器模式

--- src/main/java/top/kangyong/study01/design/pattern/filter/CriteriaPatternDemo.java
+++ src/main/java/top/kangyong/study01/design/pattern/filter/CriteriaPatternDemo.java
@@ -1,5 +1,8 @@
 package top.kangyong.study01.design.pattern.filter;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * g过滤器模式
  *
@@ -8,4 +11,15 @@ package top.kangyong.study01.design.pattern.filter;
  * @since 1.0.0
  */
 public class CriteriaPatternDemo {
+
+    public static void main(String[] args) {
+        List<Person> persons = new ArrayList<Person>();
+
+        persons.add(new Person("Robert", "Male", "Single"));
+        persons.add(new Person("John", "Male", "Married"));
+        persons.add(new Person("Laura", "Female", "Married"));
+        persons.add(new Person("Diana", "Female", "Single"));
+        persons.add(new Person("Mike", "Male", "Single"));
+        persons.add(new Person("Bobby", "Male", "Single"));
+    }
 }
