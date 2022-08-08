ecf412fbb3b0eba50437426ad36a887b2dc7030f
kangyong <kangyong_wts@sina.com>
2022-08-08 22:07:48 +0800

    feat: 创建型模式 单例模式 懒汉式安全

--- src/main/java/top/kangyong/study02/design/pattern/creatormode/singleton/demo3/Singleton.java
+++ src/main/java/top/kangyong/study02/design/pattern/creatormode/singleton/demo3/Singleton.java
@@ -17,8 +17,11 @@ public class Singleton {
 
     }
 
-    public static Singleton getInstance() {
-        instance = new Singleton();
+    public static synchronized Singleton getInstance() {
+        if (instance == null) {
+            // 如果是多线程，线程1执行到这里，线程2获取到了cpu时间片，线程2执行，就会两个对象不一样，所以要加锁处理
+            instance = new Singleton();
+        }
         return instance;
     }
 
