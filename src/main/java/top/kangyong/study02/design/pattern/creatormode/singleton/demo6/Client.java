517ceac4511c4bf3510f35e3e0a07ef710417d0f
kangyong <kangyong_wts@sina.com>
2022-08-09 14:44:48 +0800

    feat: 单例模式 饿汉式 枚举实现方式

---
+++ src/main/java/top/kangyong/study02/design/pattern/creatormode/singleton/demo6/Client.java
@@ -0,0 +1,22 @@
+package top.kangyong.study02.design.pattern.creatormode.singleton.demo6;
+
+/**
+ * 单例模式 饿汉式 枚举实现方式
+ * <p>
+ * 创建型模式
+ *
+ * @author Kang Yong
+ * @date 2022/8/9
+ * @since 1.0.0
+ */
+public class Client {
+
+    public static void main(String[] args) {
+        Singleton instance = Singleton.INSTANCE;
+        Singleton instance2 = Singleton.INSTANCE;
+
+        System.out.println(instance == instance2);
+        System.out.println(instance);
+        System.out.println(instance2);
+    }
+}
---
+++ src/main/java/top/kangyong/study02/design/pattern/creatormode/singleton/demo6/Singleton.java
@@ -0,0 +1,16 @@
+package top.kangyong.study02.design.pattern.creatormode.singleton.demo6;
+
+/**
+ * 单例模式 饿汉式 枚举实现方式
+ * <p>
+ * 创建型模式
+ *
+ * @author Kang Yong
+ * @date 2022/8/9
+ * @since 1.0.0
+ */
+public enum Singleton {
+
+    INSTANCE
+    ;
+}
