6af8ee7e987a9cfeafd8cf602b0e01bdfbd6d7ca
kangyong <kangyong_wts@sina.com>
2022-08-15 22:25:39 +0800

    feat: 组合模式

---
+++ src/main/java/top/kangyong/study01/design/pattern/composite/Employee.java
@@ -0,0 +1,19 @@
+package top.kangyong.study01.design.pattern.composite;
+
+import java.util.List;
+
+/**
+ * 组合模式
+ *
+ * @author Kang Yong
+ * @date 2022/8/15
+ * @since 1.0.0
+ */
+public class Employee {
+
+    private String name;
+    private String dept;
+    private int salary;
+    private List<Employee> subordinates;
+
+}
