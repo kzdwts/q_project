3d7dcaea52ab85eac1d398fab13a34f5f097a6a1
kangyong <kangyong_wts@sina.com>
2022-08-16 17:08:12 +0800

    feat: 正则测试

--- src/main/java/top/kangyong/study02/stringdemo/StringDemoTest.java
+++ src/main/java/top/kangyong/study02/stringdemo/StringDemoTest.java
@@ -84,5 +84,22 @@ public class StringDemoTest {
         }
     }
 
+    @Test
+    public void testRegex2() {
+
+        String pattern1 = ".*(EXPORT).*|.*(IMPORT).*";
+        String msg1 = "2022-08-16 16:16:17.060|[,,,]|[Thread-41]|INFO |com.youmatech.zhsq.operateprogress.OperateProgressMain|EXPORT|server-mobile|1206503|USERINFO|获取当前登录者信息:[{\"bguTokenId\":null,\"bguUserId\":null,\"userUuid\":null,\"userName\":null,\"platformCode\":null,\"systemCode\":null,\"token\":null,\"systemInfo\":null,\"softWare\":null,\"loginName\":null,\"orgUserId\":2331,\"orgUserUuid\":null,\"busBusinessId\":11,\"busBusinessName\":\"合生\",\"orgBusinessId\":null,\"userAdminFlag\":0}]|BODY:{\"progressTypeCode\":3,\"progressParamData\":{\"consultantCondition\":\"\",\"customerCondition\":\"\",\"syncFlagList\":[],\"customerProgressCodeList\":[],\"purposeLevelIdList\":[],\"validStatusCodeList\":[],\"consultantName\":\"\",\"customerOriginCode\":null,\"busProjectId\":78,\"distributeStatusCode\":1203531,\"followStartTime\":\"\",\"followEndTime\":\"\",\"distributeStartTime\":\"\",\"distributeEndTime\":\"\",\"invalidStartTime\":\"\",\"invalidEndTime\":\"\",\"discardStartTime\":\"\",\"discardEndTime\":\"\",\"cusCondition\":\"\"},\"progressBusinessCode\":1206503,\"accessToken\":\"eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIyMzMxIiwiYnVzQnVzaW5lc3NJZCI6MTEsImV4cGlyZXNEYXRlIjoxNjYxNTI2NDAwOTI2LCJleHAiOjE2NjE1MjY0MDB9.qzun_XGkfJIcicBKg5g25A1Hwf7LC_fRf2HyRQ2l-wnP_nEiz0PJ_i7p6PI20IPO8cA90PTG-gOdLoTVOwZicA\"}|RESP:{\"code\":3001,\"msg\":\"操作成功\",\"data\":{\"count\":6,\"excelHttpPath\":\"https://dev-jiaofu.yuanzhiyijiantong.com/resources/tmp/2022-08-16/5ac7d201-fcb8-4af5-ba6c-09030b1d0b9e/跟进中客户导出.xlsx\"},\"error\":null,\"success\":true}";
+
+        boolean m1 = Pattern.matches(pattern1, msg1);
+        System.out.println(m1);
+
+        String msg2 = "2022-08-16 16:16:17.060|[,,,]|[Thread-41]|INFO |com.youmatech.zhsq.operateprogress.OperateProgressMain|IMPORT|server-mobile|1206503|USERINFO|获取当前登录者信息:[{\"bguTokenId\":null,\"bguUserId\":null,\"userUuid\":null,\"userName\":null,\"platformCode\":null,\"systemCode\":null,\"token\":null,\"systemInfo\":null,\"softWare\":null,\"loginName\":null,\"orgUserId\":2331,\"orgUserUuid\":null,\"busBusinessId\":11,\"busBusinessName\":\"合生\",\"orgBusinessId\":null,\"userAdminFlag\":0}]|BODY:{\"progressTypeCode\":3,\"progressParamData\":{\"consultantCondition\":\"\",\"customerCondition\":\"\",\"syncFlagList\":[],\"customerProgressCodeList\":[],\"purposeLevelIdList\":[],\"validStatusCodeList\":[],\"consultantName\":\"\",\"customerOriginCode\":null,\"busProjectId\":78,\"distributeStatusCode\":1203531,\"followStartTime\":\"\",\"followEndTime\":\"\",\"distributeStartTime\":\"\",\"distributeEndTime\":\"\",\"invalidStartTime\":\"\",\"invalidEndTime\":\"\",\"discardStartTime\":\"\",\"discardEndTime\":\"\",\"cusCondition\":\"\"},\"progressBusinessCode\":1206503,\"accessToken\":\"eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIyMzMxIiwiYnVzQnVzaW5lc3NJZCI6MTEsImV4cGlyZXNEYXRlIjoxNjYxNTI2NDAwOTI2LCJleHAiOjE2NjE1MjY0MDB9.qzun_XGkfJIcicBKg5g25A1Hwf7LC_fRf2HyRQ2l-wnP_nEiz0PJ_i7p6PI20IPO8cA90PTG-gOdLoTVOwZicA\"}|RESP:{\"code\":3001,\"msg\":\"操作成功\",\"data\":{\"count\":6,\"excelHttpPath\":\"https://dev-jiaofu.yuanzhiyijiantong.com/resources/tmp/2022-08-16/5ac7d201-fcb8-4af5-ba6c-09030b1d0b9e/跟进中客户导出.xlsx\"},\"error\":null,\"success\":true}";
+
+        boolean m2 = Pattern.matches(pattern1, msg2);
+        System.out.println(m2);
+
+
+    }
+
 
 }
