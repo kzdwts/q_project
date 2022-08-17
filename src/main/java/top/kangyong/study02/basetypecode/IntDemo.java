a5d599664f872cade5c91a22ad1c5b7667dd4c83
kangyong <kangyong_wts@sina.com>
2022-08-17 17:01:11 +0800

    feat: max

---
+++ src/main/java/top/kangyong/study02/basetypecode/IntDemo.java
@@ -0,0 +1,19 @@
+package top.kangyong.study02.basetypecode;
+
+import org.junit.Test;
+
+/**
+ * test
+ *
+ * @author Kang Yong
+ * @date 2022/8/17
+ * @since 1.0.0
+ */
+public class IntDemo {
+
+    @Test
+    public void demo01() {
+        System.out.println(Integer.MAX_VALUE);
+        System.out.println(Integer.MIN_VALUE);
+    }
+}
