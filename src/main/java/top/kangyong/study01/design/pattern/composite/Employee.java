3b6f7fbadddad9f98f261838727021dc84cebaf7
kangyong <kangyong_wts@sina.com>
2022-08-18 23:37:33 +0800

    feat: 组合模式

--- src/main/java/top/kangyong/study01/design/pattern/composite/Employee.java
+++ src/main/java/top/kangyong/study01/design/pattern/composite/Employee.java
@@ -1,5 +1,6 @@
 package top.kangyong.study01.design.pattern.composite;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -16,4 +17,32 @@ public class Employee {
     private int salary;
     private List<Employee> subordinates;
 
+    public Employee(String name, String dept, int salary) {
+        this.name = name;
+        this.dept = dept;
+        this.salary = salary;
+        this.subordinates = new ArrayList<Employee>();
+    }
+
+    public void add(Employee employee) {
+        this.subordinates.add(employee);
+    }
+
+    public void remove(Employee employee) {
+        this.subordinates.remove(employee);
+    }
+
+    public List<Employee> getSubordinates() {
+        return this.subordinates;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "name='" + name + '\'' +
+                ", dept='" + dept + '\'' +
+                ", salary=" + salary +
+                ", subordinates=" + subordinates +
+                '}';
+    }
 }
