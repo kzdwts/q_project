0f18dc1850132898e03394580eb2b6dbf847f9f8
kangyong <kangyong_wts@sina.com>
2022-08-21 22:31:58 +0800

    feat: 工厂模式

---
+++ src/main/java/top/kangyong/study02/design/pattern/creatormode/factory/before/Coffee.java
@@ -0,0 +1,13 @@
+package top.kangyong.study02.design.pattern.creatormode.factory.before;
+
+/**
+ * 工厂模式
+ * <p>
+ * 创建型模式
+ *
+ * @author Kang Yong
+ * @date 2022/8/21
+ * @since 1.0.0
+ */
+public class Coffee {
+}
