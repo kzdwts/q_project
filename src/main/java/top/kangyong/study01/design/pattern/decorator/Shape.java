7f35952621784fa25ca8e8f89cc1e1da808f866a
kangyong <kangyong_wts@sina.com>
2022-08-22 09:19:53 +0800

    feat: 装饰器模式

---
+++ src/main/java/top/kangyong/study01/design/pattern/decorator/Shape.java
@@ -0,0 +1,13 @@
+package top.kangyong.study01.design.pattern.decorator;
+
+/**
+ * 装饰器模式
+ *
+ * @author Kang Yong
+ * @date 2022/8/22
+ * @since 1.0.0
+ */
+public interface Shape {
+
+    void draw();
+}
