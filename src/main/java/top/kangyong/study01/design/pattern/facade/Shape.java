285ca44fd469bfa8c7c7992c9de2e3e281d79eff
kangyong <kangyong_wts@sina.com>
2022-08-26 09:37:12 +0800

    feat: 外观模式

---
+++ src/main/java/top/kangyong/study01/design/pattern/facade/Shape.java
@@ -0,0 +1,13 @@
+package top.kangyong.study01.design.pattern.facade;
+
+/**
+ * 外观模式
+ *
+ * @author Kang Yong
+ * @date 2022/8/26
+ * @since 1.0.0
+ */
+public interface Shape {
+
+    void draw();
+}
