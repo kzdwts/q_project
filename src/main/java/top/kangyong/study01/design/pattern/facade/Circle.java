559f6a719f5bccca5724630e60400ea9141317d9
kangyong <kangyong_wts@sina.com>
2022-08-27 16:06:53 +0800

    feat: 装饰器模式

---
+++ src/main/java/top/kangyong/study01/design/pattern/facade/Circle.java
@@ -0,0 +1,15 @@
+package top.kangyong.study01.design.pattern.facade;
+
+/**
+ * 外观模式
+ *
+ * @author Kang Yong
+ * @date 2022/8/27
+ * @since 1.0.0
+ */
+public class Circle implements Shape {
+    @Override
+    public void draw() {
+        System.out.println("Circle: draw");
+    }
+}
---
+++ src/main/java/top/kangyong/study01/design/pattern/facade/Rectangle.java
@@ -0,0 +1,15 @@
+package top.kangyong.study01.design.pattern.facade;
+
+/**
+ * 外观模式
+ *
+ * @author Kang Yong
+ * @date 2022/8/27
+ * @since 1.0.0
+ */
+public class Rectangle implements Shape {
+    @Override
+    public void draw() {
+        System.out.println("Rectangle : draw");
+    }
+}
