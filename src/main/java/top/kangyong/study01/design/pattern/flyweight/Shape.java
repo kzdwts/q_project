3a7e46c8b202da0f1622ad32bba65dc8591dd7bf
kangyong <kangyong_wts@sina.com>
2022-08-30 18:58:43 +0800

    feat: 享元模式

---
+++ src/main/java/top/kangyong/study01/design/pattern/flyweight/Shape.java
@@ -0,0 +1,13 @@
+package top.kangyong.study01.design.pattern.flyweight;
+
+/**
+ * 享元模式
+ *
+ * @author Kang Yong
+ * @date 2022/8/26
+ * @since 1.0.0
+ */
+public interface Shape {
+
+    void draw();
+}
