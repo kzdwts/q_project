cc0b53c0ef22f3e462351244233a44e978334047
kangyong <kangyong_wts@sina.com>
2022-09-03 13:49:04 +0800

    feat: 享元模式

---
+++ src/main/java/top/kangyong/study01/design/pattern/flyweight/FlyweightPatternDemo.java
@@ -0,0 +1,19 @@
+package top.kangyong.study01.design.pattern.flyweight;
+
+/**
+ * 享元模式
+ * <p>
+ * 结构型模式
+ *
+ * @author Kang Yong
+ * @date 2022/9/3
+ * @since 1.0.0
+ */
+public class FlyweightPatternDemo {
+
+    public static final String colors[] = {"Red", "Green", "Blue", "White", "Black"};
+
+    public static void main(String[] args) {
+
+    }
+}
