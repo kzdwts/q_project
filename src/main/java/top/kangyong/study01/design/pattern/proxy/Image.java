d3469070c61a6e9d8e1c869c688ea4c1302ec56d
kangyong <kangyong_wts@sina.com>
2022-09-05 23:06:27 +0800

    feat: 代理模式

---
+++ src/main/java/top/kangyong/study01/design/pattern/proxy/Image.java
@@ -0,0 +1,13 @@
+package top.kangyong.study01.design.pattern.proxy;
+
+/**
+ * 代理模式
+ *
+ * @author Kang Yong
+ * @date 2022/9/5
+ * @since 1.0.0
+ */
+public interface Image {
+
+    void display();
+}
