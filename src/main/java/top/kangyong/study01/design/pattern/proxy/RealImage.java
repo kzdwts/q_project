2a238da8ba0850e7987ebe34f16a6cee2ed899cb
kangyong <kangyong_wts@sina.com>
2022-09-06 17:31:37 +0800

    feat: 结构型模式-代理模式

---
+++ src/main/java/top/kangyong/study01/design/pattern/proxy/RealImage.java
@@ -0,0 +1,17 @@
+package top.kangyong.study01.design.pattern.proxy;
+
+/**
+ * 代理模式
+ * <p>
+ * 结构型模式
+ *
+ * @author Kang Yong
+ * @date 2022/9/6
+ * @since 1.0.0
+ */
+public class RealImage implements Image {
+    @Override
+    public void display() {
+
+    }
+}
