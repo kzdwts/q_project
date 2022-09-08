7ec533ddd2d86cb9978d98d4345b01bf00d9ce03
kangyong <kangyong_wts@sina.com>
2022-09-08 14:00:02 +0800

    feat: 责任链模式

---
+++ src/main/java/top/kangyong/study01/design/pattern/chain/AbstractLogger.java
@@ -0,0 +1,37 @@
+package top.kangyong.study01.design.pattern.chain;
+
+/**
+ * 责任链模式
+ *
+ * @author Kang Yong
+ * @date 2022/9/8
+ * @since 1.0.0
+ */
+public abstract class AbstractLogger {
+
+    private static int INFO = 1;
+    private static int DEBUG = 2;
+    private static int EROR = 3;
+
+    protected int level;
+
+    // 责任链中的下一个元素
+    protected AbstractLogger nextLogger;
+
+    public void setNextLogger(AbstractLogger nextLogger) {
+        this.nextLogger = nextLogger;
+    }
+
+    public void logMessage(int level, String message) {
+        if (this.level <= level) {
+            write(message);
+        }
+
+        if (nextLogger != null) {
+            nextLogger.logMessage(level, message);
+        }
+    }
+
+    abstract protected void write(String message);
+
+}
---
+++ src/main/java/top/kangyong/study01/json/CityDataDemo.java
@@ -0,0 +1,19 @@
+package top.kangyong.study01.json;
+
+import org.junit.Test;
+
+/**
+ * 城市数据处理
+ *
+ * @author Kang Yong
+ * @date 2022/9/8
+ * @since 1.0.0
+ */
+public class CityDataDemo {
+
+    @Test
+    public void demo01() {
+
+
+    }
+}
