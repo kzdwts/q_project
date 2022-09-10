225626b09e8cc1c7bb6f83431379bf2d3ef0dff7
kangyong <kangyong_wts@sina.com>
2022-09-10 22:36:57 +0800

    feat: 责任链模式

---
+++ src/main/java/top/kangyong/study01/design/pattern/chain/ErrorLogger.java
@@ -0,0 +1,22 @@
+package top.kangyong.study01.design.pattern.chain;
+
+/**
+ * 责任链模式
+ * <p>
+ * 行为型模式
+ *
+ * @author Kang Yong
+ * @date 2022/9/10
+ * @since 1.0.0
+ */
+public class ErrorLogger extends AbstractLogger {
+
+    public ErrorLogger(int level) {
+        this.level = level;
+    }
+
+    @Override
+    protected void write(String message) {
+        System.out.println("Error Console Logger: " + message);
+    }
+}
