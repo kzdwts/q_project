f331d77cf382f7258988ba75c1d34348ca811094
kangyong <kangyong_wts@sina.com>
2022-09-13 15:17:49 +0800

    feat: 行为型模式-命令模式

---
+++ src/main/java/top/kangyong/study01/design/pattern/command/Order.java
@@ -0,0 +1,15 @@
+package top.kangyong.study01.design.pattern.command;
+
+/**
+ * 命令模式
+ * <p>
+ * 行为型模式
+ *
+ * @author Kang Yong
+ * @date 2022/9/13
+ * @since 1.0.0
+ */
+public interface Order {
+
+    void execute();
+}
