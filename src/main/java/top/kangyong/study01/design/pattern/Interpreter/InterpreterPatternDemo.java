b439b60296082bb82d88d308d211b977c5aecb39
kangyong <kangyong_wts@sina.com>
2022-09-17 23:08:39 +0800

    feat: 解释器模式

---
+++ src/main/java/top/kangyong/study01/design/pattern/Interpreter/InterpreterPatternDemo.java
@@ -0,0 +1,17 @@
+package top.kangyong.study01.design.pattern.Interpreter;
+
+/**
+ * 解释器模式
+ * <p>
+ * 行为型模式
+ *
+ * @author Kang Yong
+ * @date 2022/9/17
+ * @since 1.0.0
+ */
+public class InterpreterPatternDemo {
+
+    public static void main(String[] args) {
+
+    }
+}
