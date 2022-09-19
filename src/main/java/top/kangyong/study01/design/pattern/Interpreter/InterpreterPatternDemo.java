3e9949520445eeda1b171d0f93c4eac263dc0869
kangyong <kangyong_wts@sina.com>
2022-09-19 13:49:11 +0800

    feat: 行为型模式-解释器模式

--- src/main/java/top/kangyong/study01/design/pattern/Interpreter/InterpreterPatternDemo.java
+++ src/main/java/top/kangyong/study01/design/pattern/Interpreter/InterpreterPatternDemo.java
@@ -25,6 +25,19 @@ public class InterpreterPatternDemo {
         return new OrExpression(robot, john);
     }
 
+    /**
+     * 规则：julie是个已婚女性
+     *
+     * @return {@link Expression}
+     * @author Kang Yong
+     * @date 2022/9/19
+     */
+    public static Expression getMarriedWomanExpression() {
+        TerminalExpression julie = new TerminalExpression("julie");
+        TerminalExpression married = new TerminalExpression("married");
+        return new AndExpression(julie, married);
+    }
+
     public static void main(String[] args) {
 
     }
