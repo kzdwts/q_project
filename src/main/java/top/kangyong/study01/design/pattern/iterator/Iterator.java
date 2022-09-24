fd5c695eccf73027f67268285c243c69d7705a93
kangyong <kangyong_wts@sina.com>
2022-09-24 09:40:17 +0800

    feat: 迭代器模式

---
+++ src/main/java/top/kangyong/study01/design/pattern/iterator/Iterator.java
@@ -0,0 +1,16 @@
+package top.kangyong.study01.design.pattern.iterator;
+
+/**
+ * 迭代器模式
+ *
+ * @author Kang Yong
+ * @date 2022/9/24
+ * @since 1.0.0
+ */
+public interface Iterator {
+
+    boolean hasNext();
+
+    Object next();
+
+}
