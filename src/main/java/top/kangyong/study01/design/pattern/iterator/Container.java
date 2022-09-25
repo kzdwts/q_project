2800a73331659d59dd7849b26065ecb89af1282e
kangyong <kangyong_wts@sina.com>
2022-09-25 13:21:43 +0800

    feat: 迭代器模式

---
+++ src/main/java/top/kangyong/study01/design/pattern/iterator/Container.java
@@ -0,0 +1,13 @@
+package top.kangyong.study01.design.pattern.iterator;
+
+/**
+ * 迭代器模式
+ *
+ * @author Kang Yong
+ * @date 2022/9/25
+ * @since 1.0.0
+ */
+public interface Container {
+
+    Iterator getIterator();
+}
