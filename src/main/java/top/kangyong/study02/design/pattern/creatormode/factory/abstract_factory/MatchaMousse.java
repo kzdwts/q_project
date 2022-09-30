eee364eb75291df945813cf3d82df40aa48f9f4e
kangyong <kangyong_wts@sina.com>
2022-09-30 22:18:51 +0800

    feat: 抽象工厂

---
+++ src/main/java/top/kangyong/study02/design/pattern/creatormode/factory/abstract_factory/MatchaMousse.java
@@ -0,0 +1,15 @@
+package top.kangyong.study02.design.pattern.creatormode.factory.abstract_factory;
+
+/**
+ * 抹茶慕斯
+ *
+ * @author Kang Yong
+ * @date 2022/9/27
+ * @since 1.0.0
+ */
+public class MatchaMousse extends Dessert {
+    @Override
+    public void show() {
+        System.out.println("抹茶慕斯");
+    }
+}
