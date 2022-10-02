efe1b4fa1798c9d18ea36b2faeb1520c60732ba9
kangyong <kangyong_wts@sina.com>
2022-10-02 22:46:16 +0800

    feat: 迭代器模式

---
+++ src/main/java/top/kangyong/study01/design/pattern/iterator/IteratorPatternDemo.java
@@ -0,0 +1,11 @@
+package top.kangyong.study01.design.pattern.iterator;
+
+/**
+ * 迭代器模式
+ *
+ * @author Kang Yong
+ * @date 2022/10/2
+ * @since 1.0.0
+ */
+public class IteratorPatternDemo {
+}
--- src/main/java/top/kangyong/study01/design/pattern/iterator/NameRepository.java
+++ src/main/java/top/kangyong/study01/design/pattern/iterator/NameRepository.java
@@ -16,13 +16,21 @@ public class NameRepository implements Container {
 
     private class NameIterator implements Iterator {
 
+        int index;
+
         @Override
         public boolean hasNext() {
+            if (index < names.length) {
+                return true;
+            }
             return false;
         }
 
         @Override
         public Object next() {
+            if (this.hasNext()) {
+                return names[index++];
+            }
             return null;
         }
     }
