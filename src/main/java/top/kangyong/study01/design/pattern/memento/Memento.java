be1ca940b6e7e25d0bbdda7bb8083772b969b1da
kangyong <kangyong_wts@sina.com>
2022-10-05 22:01:49 +0800

    feat: 备忘录模式

---
+++ src/main/java/top/kangyong/study01/design/pattern/memento/Memento.java
@@ -0,0 +1,22 @@
+package top.kangyong.study01.design.pattern.memento;
+
+/**
+ * 备忘录模式
+ *
+ * @author Kang Yong
+ * @date 2022/10/5
+ * @since 1.0.0
+ */
+public class Memento {
+
+    private String state;
+
+    public Memento(String state) {
+        this.state = state;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+}
