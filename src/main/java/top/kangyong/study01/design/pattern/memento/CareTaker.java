945f90a62cb0d8ea4a0d62a4a5ac0f18cedd7e6f
kangyong <kangyong_wts@sina.com>
2022-10-09 15:49:47 +0800

    feat: 备忘录模式

---
+++ src/main/java/top/kangyong/study01/design/pattern/memento/CareTaker.java
@@ -0,0 +1,24 @@
+package top.kangyong.study01.design.pattern.memento;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 备忘录模式
+ *
+ * @author Kang Yong
+ * @date 2022/10/9
+ * @since 1.0.0
+ */
+public class CareTaker {
+
+    private List<Memento> mementoList = new ArrayList<>();
+
+    public void add(Memento memento) {
+        mementoList.add(memento);
+    }
+
+    public Memento get(int index) {
+        return mementoList.get(index);
+    }
+}
