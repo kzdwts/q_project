4caf31fa7d3dc7f3605fcd0a1d91f08952848dde
kangyong <kangyong_wts@sina.com>
2022-10-11 09:39:13 +0800

    feat: 日期每天

--- src/main/java/top/kangyong/study02/datecode/DateUtil.java
+++ src/main/java/top/kangyong/study02/datecode/DateUtil.java
@@ -1,9 +1,7 @@
 package top.kangyong.study02.datecode;
 
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.List;
-import java.util.Locale;
+import java.time.LocalDate;
+import java.util.*;
 
 /**
  * 日期工具类
@@ -25,11 +23,33 @@ public class DateUtil {
         List<String> resultList = new ArrayList<>();
         Calendar calendar = Calendar.getInstance(Locale.CHINA);
         int year = calendar.get(Calendar.YEAR);
-        int month = calendar.get(Calendar.MONTH);
+        int month = calendar.get(Calendar.MONTH) + 1;
         int day = calendar.getActualMaximum(Calendar.DATE);
         for (int i = 1; i <= day; i++) {
-            String date = String.valueOf(year) + "-" + month + "-" + i;
-            resultList.add(date);
+            String dateStr = String.valueOf(year) + "-" + month + "-" + i;
+            resultList.add(dateStr);
+        }
+        return resultList;
+    }
+
+    /**
+     * 获取当前时间所在月的每一天
+     *
+     * @param date {@link Date}
+     * @return {@link List< String>}
+     * @author Kang Yong
+     * @date 2022/10/11
+     */
+    public static List<String> getDayListOfMonth(Date date) {
+        List<String> resultList = new ArrayList<>();
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH) + 1;
+        int day = calendar.getActualMaximum(Calendar.DATE);
+        for (int i = 1; i <= day; i++) {
+            String dateStr = String.valueOf(year) + "-" + month + "-" + i;
+            resultList.add(dateStr);
         }
         return resultList;
     }
--- src/main/java/top/kangyong/study02/datecode/DateUtilTest.java
+++ src/main/java/top/kangyong/study02/datecode/DateUtilTest.java
@@ -2,6 +2,7 @@ package top.kangyong.study02.datecode;
 
 import org.junit.Test;
 
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -16,6 +17,12 @@ public class DateUtilTest {
     @Test
     public void testGetDayListOfMonth() {
         List<String> list = DateUtil.getDayListOfMonth();
+        list.forEach(System.out::println);
+    }
+
+    @Test
+    public void testGetDayListOfMonth2() {
+        List<String> list = DateUtil.getDayListOfMonth(new Date());
         list.forEach(System.out::println);
     }
 
