d48fed9d47b05bf8cfe36463565c5940e214093c
kangyong <kangyong_wts@sina.com>
2022-10-21 22:14:31 +0800

    feat: 状态模式

--- src/main/java/top/kangyong/study01/design/pattern/state/StopState.java
+++ src/main/java/top/kangyong/study01/design/pattern/state/StopState.java
@@ -4,19 +4,18 @@ package top.kangyong.study01.design.pattern.state;
  * 状态模式
  *
  * @author Kang Yong
- * @date 2022/10/20
+ * @date 2022/10/21
  * @since 1.0.0
  */
-public class StartState implements State {
-
+public class StopState implements State {
     @Override
     public void doAction(Context context) {
-        System.out.println("player is in start state");
+        System.out.println("player is in stop state");
         context.setState(this);
     }
 
     @Override
     public String toString() {
-        return "Start State";
+        return "Stop State";
     }
 }
