df5fb6b5b35a34f14ffb86b4aeee5292820bcf26
kangyong <kangyong_wts@sina.com>
2022-10-29 14:14:42 +0800

    feat: 代理模式-cglib动态代理

---
+++ src/main/java/top/kangyong/study02/design/pattern/structuralmode/proxy/cglib_proxy/TrainStation.java
@@ -0,0 +1,15 @@
+package top.kangyong.study02.design.pattern.structuralmode.proxy.cglib_proxy;
+
+/**
+ * 火车站售票
+ *
+ * @author Kang Yong
+ * @date 2022/10/29
+ * @since 1.0.0
+ */
+public class TrainStation {
+
+    public void sell() {
+        System.out.println("火车站卖票");
+    }
+}
