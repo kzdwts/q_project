8b02064632503f37479e5efea334754eb25145f6
kangyong <kangyong_wts@sina.com>
2022-11-09 21:56:04 +0800

    feat: 装饰器模式更新

--- src/main/java/top/kangyong/study01/design/pattern/decorator/DecoratorPatternDemo.java
+++ src/main/java/top/kangyong/study01/design/pattern/decorator/DecoratorPatternDemo.java
@@ -12,7 +12,7 @@ public class DecoratorPatternDemo {
     public static void main(String[] args) {
         Shape circle = new Circle();
         ShapeDecorator redCircle = new RedShapeDecorator(new Circle());
-        ShapeDecorator redRectangle = new RedShapeDecorator(new Circle());
+        ShapeDecorator redRectangle = new RedShapeDecorator(new Rectangle());
 
         System.out.println("Circle with normal border");
         circle.draw();
