b87b36bc4b24acec315164892755512150149fa8
kangyong <kangyong_wts@sina.com>
2022-11-11 22:52:18 +0800

    feat: 桥接模式

---
+++ src/main/java/top/kangyong/study02/design/pattern/structuralmode/bridge/AviFile.java
@@ -0,0 +1,17 @@
+package top.kangyong.study02.design.pattern.structuralmode.bridge;
+
+/**
+ * avi视频文件（具体的实现化角色）
+ *
+ * @author Kang Yong
+ * @date 2022/11/11
+ * @since 1.0.0
+ */
+public class AviFile implements VideoFile {
+
+    @Override
+    public void decode(String fileName) {
+        System.out.println("avi视频文件：" + fileName);
+    }
+
+}
