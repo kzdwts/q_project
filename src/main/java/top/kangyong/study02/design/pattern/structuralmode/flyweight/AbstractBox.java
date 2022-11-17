30a2a97c39d40f8f933531dbeec80190ac5bd514
kangyong <kangyong_wts@sina.com>
2022-11-17 23:15:17 +0800

    feat: 享元模式

---
+++ src/main/java/top/kangyong/study02/design/pattern/structuralmode/flyweight/AbstractBox.java
@@ -0,0 +1,22 @@
+package top.kangyong.study02.design.pattern.structuralmode.flyweight;
+
+/**
+ * 抽象享元角色
+ * <p>
+ * 享元模式
+ *
+ * @author Kang Yong
+ * @date 2022/11/17
+ * @since 1.0.0
+ */
+public abstract class AbstractBox {
+
+    /**
+     * 获取图形
+     *
+     * @return {@link String}
+     * @author Kang Yong
+     * @date 2022/11/17
+     */
+    public abstract String getShape();
+}
