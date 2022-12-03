cd5f2be1e9b77451f22fa22512253e63c616eed6
kangyong <kangyong_wts@sina.com>
2022-12-03 23:01:50 +0800

    feat: 数据访问对象模式

---
+++ src/main/java/top/kangyong/study01/design/pattern/data_access_object/StudentDaoImpl.java
@@ -0,0 +1,13 @@
+package top.kangyong.study01.design.pattern.data_access_object;
+
+/**
+ * 实现接口的实体
+ * <p>
+ * 数据访问对象模式
+ *
+ * @author Kang Yong
+ * @date 2022/12/3
+ * @since 1.0.0
+ */
+public class StudentDaoImpl {
+}
