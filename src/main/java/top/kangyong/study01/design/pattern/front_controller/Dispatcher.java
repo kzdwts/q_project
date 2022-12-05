6a7f622d299dc1979124f474f2713c7d952a7fe7
kangyong <kangyong_wts@sina.com>
2022-12-05 21:57:35 +0800

    feat: 前端控制器模式

---
+++ src/main/java/top/kangyong/study01/design/pattern/front_controller/Dispatcher.java
@@ -0,0 +1,30 @@
+package top.kangyong.study01.design.pattern.front_controller;
+
+/**
+ * 调度器
+ * <p>
+ * 前端控制器模式
+ *
+ * @author Kang Yong
+ * @date 2022/12/5
+ * @since 1.0.0
+ */
+public class Dispatcher {
+
+    private HomeView homeView;
+    private StudentView studentView;
+
+    public Dispatcher(HomeView homeView, StudentView studentView) {
+        this.homeView = homeView;
+        this.studentView = studentView;
+    }
+
+    public void dispatch(String request) {
+        if (request.equalsIgnoreCase("STUDENT")) {
+            studentView.show();
+        } else {
+            homeView.show();
+        }
+    }
+
+}
---
+++ src/main/java/top/kangyong/study01/design/pattern/front_controller/HomeView.java
@@ -0,0 +1,18 @@
+package top.kangyong.study01.design.pattern.front_controller;
+
+/**
+ * 视图
+ * <p>
+ * 前端控制器模式
+ *
+ * @author Kang Yong
+ * @date 2022/12/5
+ * @since 1.0.0
+ */
+public class HomeView {
+
+    public void show() {
+        System.out.println("Displaying home page");
+    }
+
+}
