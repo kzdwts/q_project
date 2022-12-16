7adb93c5c21886b252114922ba7342561c857106
kangyong <kangyong_wts@sina.com>
2022-12-16 10:38:44 +0800

    feat: 服务定位器模式

--- src/main/java/top/kangyong/study01/design/pattern/service_locator/ServiceLocator.java
+++ src/main/java/top/kangyong/study01/design/pattern/service_locator/ServiceLocator.java
@@ -10,4 +10,23 @@ package top.kangyong.study01.design.pattern.service_locator;
  * @since 1.0.0
  */
 public class ServiceLocator {
+
+    private static Cache cache;
+
+    static {
+        cache = new Cache();
+    }
+
+    public Service getService(String jndiName) {
+        Service service = cache.getService(jndiName);
+        if (service != null) {
+            return service;
+        }
+
+        InitialContext context = new InitialContext();
+        Service lookupService = (Service) context.lookup(jndiName);
+        cache.addService(lookupService);
+
+        return lookupService;
+    }
 }
