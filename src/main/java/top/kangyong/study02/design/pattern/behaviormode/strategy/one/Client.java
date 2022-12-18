c315101defd2318032379ea2a01bfac27536ef42
kangyong <kangyong_wts@sina.com>
2022-12-18 22:28:24 +0800

    feat: 策略模式优缺点描述

--- src/main/java/top/kangyong/study02/design/pattern/behaviormode/strategy/one/Client.java
+++ src/main/java/top/kangyong/study02/design/pattern/behaviormode/strategy/one/Client.java
@@ -3,7 +3,15 @@ package top.kangyong.study02.design.pattern.behaviormode.strategy.one;
 /**
  * test
  * <p>
- * 策略模式
+ * 策略模式：
+ * 优点：
+ * 策略类之间可以自由切换
+ * 易于扩展
+ * 避免使用多重条件选择语句（if-else），充分体现面向对象设计思想
+ * <p>
+ * 缺点：
+ * 客户端必须知道所有的策略类，并自行决定使用哪一个策略类
+ * 策略模式将造成很多策略类，可以通过使用享元模式在一定程度上减少对象的策略
  *
  * @author Kang Yong
  * @date 2022/12/1
