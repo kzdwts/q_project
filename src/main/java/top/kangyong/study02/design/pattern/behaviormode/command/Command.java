125cddecb3ba768634a3283ad5353a4face713aa
kangyong <kangyong_wts@sina.com>
2022-12-25 15:46:50 +0800

    feat: 命令模式

---
+++ src/main/java/top/kangyong/study02/design/pattern/behaviormode/command/Command.java
@@ -0,0 +1,15 @@
+package top.kangyong.study02.design.pattern.behaviormode.command;
+
+/**
+ * 抽象命令类
+ * <p>
+ * 命令模式
+ *
+ * @author Kang Yong
+ * @date 2022/12/25
+ * @since 1.0.0
+ */
+public interface Command {
+
+    void execute();
+}
---
+++ src/main/java/top/kangyong/study02/design/pattern/behaviormode/command/OrderCommand.java
@@ -0,0 +1,36 @@
+package top.kangyong.study02.design.pattern.behaviormode.command;
+
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * 订单命令类
+ * <p>
+ * 命令模式
+ *
+ * @author Kang Yong
+ * @date 2022/12/25
+ * @since 1.0.0
+ */
+public class OrderCommand implements Command {
+
+    private SeniorChef receiver;
+    private Order order;
+
+    public OrderCommand(SeniorChef receiver, Order order) {
+        this.receiver = receiver;
+        this.order = order;
+    }
+
+    @Override
+    public void execute() {
+        System.out.println(order.getDiningTable() + "桌的订单：");
+        Map<String, Integer> foodDir = order.getFoodDir();
+        Set<String> keys = foodDir.keySet();
+        for (String foodName : keys) {
+            receiver.makeFood(foodName, foodDir.get(foodName));
+        }
+
+        System.out.println(order.getDiningTable() + "桌的饭已完成");
+    }
+}
