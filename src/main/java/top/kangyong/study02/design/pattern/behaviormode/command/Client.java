fc273713045100955e3ddab0811c1a766e0433f5
kangyong <kangyong_wts@sina.com>
2022-12-26 14:46:38 +0800

    feat: 命令模式描述

--- src/main/java/top/kangyong/study02/design/pattern/behaviormode/command/Client.java
+++ src/main/java/top/kangyong/study02/design/pattern/behaviormode/command/Client.java
@@ -4,6 +4,15 @@ package top.kangyong.study02.design.pattern.behaviormode.command;
  * test
  * <p>
  * 命令模式
+ * <p>
+ * 优点：
+ *  降低系统耦合度
+ *  增加或删除命令非常方便
+ *  可以实现宏命令
+ *  方便实现undo和redo操作
+ * 缺点：
+ *  使用命令模式可能会导致某些系统有过多的具体命令类
+ *  系统结构更加复杂
  *
  * @author Kang Yong
  * @date 2022/12/25
