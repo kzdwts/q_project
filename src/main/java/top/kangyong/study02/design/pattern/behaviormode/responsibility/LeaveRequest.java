c95e75118d9b6990653245a4c18fabd3714c292f
kangyong <kangyong_wts@sina.com>
2022-12-26 15:07:08 +0800

    feat: 责任链模式

---
+++ src/main/java/top/kangyong/study02/design/pattern/behaviormode/responsibility/LeaveRequest.java
@@ -0,0 +1,13 @@
+package top.kangyong.study02.design.pattern.behaviormode.responsibility;
+
+/**
+ * 请假条
+ * <p>
+ * 责任链模式
+ *
+ * @author Kang Yong
+ * @date 2022/12/26
+ * @since 1.0.0
+ */
+public class LeaveRequest {
+}
