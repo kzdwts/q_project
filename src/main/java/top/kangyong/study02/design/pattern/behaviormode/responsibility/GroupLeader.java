ebb2366aec1b3c989cf17e23eb78337e995b60c6
kangyong <kangyong_wts@sina.com>
2022-12-27 21:32:25 +0800

    feat: 责任链模式

---
+++ src/main/java/top/kangyong/study02/design/pattern/behaviormode/responsibility/GroupLeader.java
@@ -0,0 +1,13 @@
+package top.kangyong.study02.design.pattern.behaviormode.responsibility;
+
+/**
+ * 小组长类（具体的处理者）
+ * <p>
+ * 责任链模式
+ *
+ * @author Kang Yong
+ * @date 2022/12/27
+ * @since 1.0.0
+ */
+public class GroupLeader {
+}
