25d81910f108fdb1f11e6e7f1c45eede2dea4421
kangyong <kangyong_wts@sina.com>
2022-12-31 15:21:33 +0800

    feat: 责任链模式-优点

--- src/main/java/top/kangyong/study02/design/pattern/behaviormode/responsibility/Client.java
+++ src/main/java/top/kangyong/study02/design/pattern/behaviormode/responsibility/Client.java
@@ -4,7 +4,12 @@ package top.kangyong.study02.design.pattern.behaviormode.responsibility;
  * test
  * <p>
  * 责任链模式
- *
+ * 优点：
+ * 降低了对象之间的耦合度
+ * 增强了系统的可扩展性
+ * 增强了给对象指派职责的灵活性
+ * 责任链简化了对象之间的连接
+ * 责任分担
  * @author Kang Yong
  * @date 2022/12/30
  * @since 1.0.0
