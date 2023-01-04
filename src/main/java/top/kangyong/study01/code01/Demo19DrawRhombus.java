a00875fd387f93121912903d07e587d6a51a404f
kangyong <kangyong_wts@sina.com>
2023-01-04 17:50:39 +0800

    feat: 画菱形

--- src/main/java/top/kangyong/study01/code01/Demo19DrawRhombus.java
+++ src/main/java/top/kangyong/study01/code01/Demo19DrawRhombus.java
@@ -11,24 +11,38 @@ package top.kangyong.study01.code01;
 public class Demo19DrawRhombus {
 
     public static void main(String[] args) {
-//   *
-//  ***
-// *****
-//*******
-// *****
-//  ***
-//   *
+//   *              1-4
+//  ***             2-345
+// *****            3-23456
+//*******           4-1234567
+// *****            5-23456
+//  ***             6-345
+//   *              7-4
 
         int h = 7;
         int w = 7;
-        for (int i = 0; i < h; i++) {
+        for (int i = 0; i < (h + 1) / 2; i++) {
 
-            for (int j = 0; j < w; j++) {
-                if (j < w/2) {
-
-                }
+            for (int j = 0; j < w / 2 - i; j++) {
+                System.out.print(" ");
             }
 
+            for (int k = 1; k < (i + 1) * 2; k++) {
+                System.out.print('*');
+            }
+
+            System.out.println();
+        }
+
+        for (int i = 1; i <= h / 2; i++) {
+
+            for (int j = 1; j <= i; j++) {
+                System.out.print(" ");
+            }
+
+            for (int k = 1; k <= w - 2 * i; k++) {
+                System.out.print('*');
+            }
 
             System.out.println();
         }
