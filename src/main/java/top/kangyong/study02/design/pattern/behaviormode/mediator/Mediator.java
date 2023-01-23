f71db6100a065b806e50269518ee317ebf147988
kangyong <kangyong_wts@sina.com>
2023-01-23 21:41:41 +0800

    feat: 中介者模式

---
+++ src/main/java/top/kangyong/study02/design/pattern/behaviormode/mediator/Mediator.java
@@ -0,0 +1,16 @@
+package top.kangyong.study02.design.pattern.behaviormode.mediator;
+
+/**
+ * 抽象中介者类
+ * <p>
+ * 中介者模式
+ *
+ * @author Kang Yong
+ * @date 2023/1/23
+ * @since 1.0.0
+ */
+public abstract class Mediator {
+
+    public abstract void constact(String message, Person person);
+
+}
---
+++ src/main/java/top/kangyong/study02/design/pattern/behaviormode/mediator/Person.java
@@ -0,0 +1,13 @@
+package top.kangyong.study02.design.pattern.behaviormode.mediator;
+
+/**
+ * 抽象同事类
+ * <p>
+ * 中介者模式
+ *
+ * @author Kang Yong
+ * @date 2023/1/23
+ * @since 1.0.0
+ */
+public abstract class Person {
+}
