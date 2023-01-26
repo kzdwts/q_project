de0a4237cfb6b7dfe1ea7aa7e47388fc571f326c
kangyong <kangyong_wts@sina.com>
2023-01-26 22:57:34 +0800

    feat: 中介者模式

---
+++ src/main/java/top/kangyong/study02/design/pattern/behaviormode/mediator/MediatorStructure.java
@@ -0,0 +1,37 @@
+package top.kangyong.study02.design.pattern.behaviormode.mediator;
+
+/**
+ * 具体的中介者角色类
+ *
+ * @author Kang Yong
+ * @date 2023/1/26
+ * @since 1.0.0
+ */
+public class MediatorStructure extends Mediator {
+
+    // 聚合房主和租房者对象
+
+    private HouseOwner houseOwner;
+    private Tenant tenant;
+
+    @Override
+    public void constact(String message, Person person) {
+
+    }
+
+    public HouseOwner getHouseOwner() {
+        return houseOwner;
+    }
+
+    public void setHouseOwner(HouseOwner houseOwner) {
+        this.houseOwner = houseOwner;
+    }
+
+    public Tenant getTenant() {
+        return tenant;
+    }
+
+    public void setTenant(Tenant tenant) {
+        this.tenant = tenant;
+    }
+}
