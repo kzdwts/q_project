aa8d32d9ad4d1a8e9693836820396575ded67bd6
kangyong <kangyong_wts@sina.com>
2023-05-10 23:03:05 +0800

    feat: 访问者模式

---
+++ src/main/java/top/kangyong/study02/design/pattern/behaviormode/visitor/Home.java
@@ -0,0 +1,13 @@
+package top.kangyong.study02.design.pattern.behaviormode.visitor;
+
+/**
+ * 对象结构类
+ * <p>
+ * 行为型模式-访问者模式
+ *
+ * @author Kang Yong
+ * @date 2023/5/10
+ * @since 1.0.0
+ */
+public class Home {
+}
