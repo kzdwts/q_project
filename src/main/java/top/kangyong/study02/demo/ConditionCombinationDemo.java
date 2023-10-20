94fe198bf319977cf0543d634efe2a1418e7f9b6
康永 <kangyong@youmatech.com>
2023-10-20 10:26:09 +0800

    feat: 条件组合（笛卡尔积）

--- src/main/java/top/kangyong/study02/demo/ConditionCombinationDemo.java
+++ src/main/java/top/kangyong/study02/demo/ConditionCombinationDemo.java
@@ -1,13 +1,15 @@
 package top.kangyong.study02.demo;
 
+import lombok.Data;
+
 import java.util.ArrayList;
 import java.util.List;
 
 /**
  * 现在有8个查询条件，都是下拉框选择项，有字符串（String）及数字（Integer）类型。
- * 比如：条件1（全部、选项1、选项2、选项3），
- *      条件2（全部、选项1、选项2、选项3），
- *      条件3（全部、138，246），以此类推。
+ * 比如：条件1（0、选项1、选项2、选项3），
+ * 条件2（0、选项1、选项2、选项3），
+ * 条件3（0、138，246），以此类推。
  * 我想要得到这8个条件组成的笛卡尔积，用Java代码该怎么写
  *
  * @author Kang Yong
@@ -15,48 +17,113 @@ import java.util.List;
  * @since 2.0.0
  */
 public class ConditionCombinationDemo {
+
     public static void main(String[] args) {
-        // 8个查询条件的选项
-        Object[] areaOptions = {"全部", "A", "B"};
-        Object[] cityOptions = {"全部", "A1", "A2", "A3"};
-        Object[] projectOptions = {"全部", "X", "Y"};
-        Object[] businessOptions = {"不选", 138, 246};
-        Object[] option5 = {"不选", 100, 200};
-        Object[] option6 = {"不选", "A", "B"};
-        Object[] option7 = {"不选", 10, 20};
-        Object[] option8 = {"不选", "X", "Y"};
+        List<ProjectOverviewConditionDTO> result = new ArrayList<>();
+        generateCombinations(result, new ProjectOverviewConditionDTO(), 0);
 
-        List<Object[]> conditions = new ArrayList<>();
-        conditions.add(areaOptions);
-        conditions.add(cityOptions);
-        conditions.add(projectOptions);
-        conditions.add(businessOptions);
-        conditions.add(option5);
-        conditions.add(option6);
-        conditions.add(option7);
-        conditions.add(option8);
-
-        List<List<Object>> result = new ArrayList<>();
-        cartesianProduct(result, conditions, new ArrayList<>(), 0);
-
-        // 打印所有可能的组合
-        for (List<Object> combination : result) {
-            System.out.println(combination);
+        // 打印或使用生成的条件
+        for (ProjectOverviewConditionDTO condition : result) {
+            System.out.println(condition);
         }
+
+        System.out.println("result.size() = " + result.size());
     }
 
-    public static void cartesianProduct(List<List<Object>> result, List<Object[]> conditions, List<Object> current, int index) {
-        if (index == conditions.size()) {
-            result.add(new ArrayList<>(current));
+    public static void generateCombinations(List<ProjectOverviewConditionDTO> result, ProjectOverviewConditionDTO currentCondition, int currentIndex) {
+        if (currentIndex == 7) {
+            result.add(currentCondition.clone()); // 克隆条件并将其添加到结果列表
             return;
         }
 
-        Object[] options = conditions.get(index);
+        // 为每个条件定义选项
+        List<Object> options = new ArrayList<>();
+        if (currentIndex == 0) {
+            options.add("0");  // 不选
+            options.add("A");
+            options.add("B");
+            // 添加其他区域选项
+        } else if (currentIndex == 1) {
+            // 根据选择的区域添加城市选项
+            String selectedRegion = currentCondition.getRegion();
+            if (selectedRegion == null || selectedRegion.equals("0")) {
+                options.add("0");  // 不选
+            } else if (selectedRegion.equals("A")) {
+                options.add("0");  // 不选
+                options.add("A1");
+                options.add("A2");
+                // 添加区域A下的其他城市选项
+            }
+            // 添加其他区域的城市选项
+        } else if (currentIndex == 2) {
+            options.add("0");  // 不选
+            options.add("138");
+            options.add("246");
+        } else if (currentIndex == 3) {
+            options.add(0);  // 不选
+            options.add(111);
+            options.add(222);
+        } else if (currentIndex == 4) {
+            // 添加年份选项，如：当前年份为2023，添加2022和2023
+            int currentYear = 2023;
+            options.add(currentYear - 1);
+            options.add(currentYear);
+        } else if (currentIndex == 5) {
+            options.add(0);  // 不选
+            options.add(101);
+            options.add(102);
+        } else if (currentIndex == 6) {
+            options.add(1);  // 默认选择
+            options.add(2);
+        }
+
+        // 递归生成每个选项的条件
         for (Object option : options) {
-            current.add(option);
-            cartesianProduct(result, conditions, current, index + 1);
-            current.remove(current.size() - 1);
+            if (currentIndex == 0) {
+                currentCondition.setRegion((String) option);
+            } else if (currentIndex == 1) {
+                currentCondition.setCity((String) option);
+            } else if (currentIndex == 2) {
+                currentCondition.setBusiness((String) option);
+            } else if (currentIndex == 3) {
+                currentCondition.setDecorationType((Integer) option);
+            } else if (currentIndex == 4) {
+                currentCondition.setYear((Integer) option);
+            } else if (currentIndex == 5) {
+                currentCondition.setAssessment((Integer) option);
+            } else if (currentIndex == 6) {
+                currentCondition.setWeighted((Integer) option);
+            }
+
+            generateCombinations(result, currentCondition, currentIndex + 1);
         }
     }
+}
+
+@Data
+class ProjectOverviewConditionDTO {
+    private String region;
+    private String city;
+    private String business;
+    private Integer decorationType;
+    private Integer year;
+    private Integer assessment;
+    private Integer weighted;
+
+    // Getter and setter methods for the fields
+    // Clone method to create a copy of the object
+
+    public ProjectOverviewConditionDTO clone() {
+        ProjectOverviewConditionDTO clonedCondition = new ProjectOverviewConditionDTO();
+        clonedCondition.setRegion(this.region);
+        clonedCondition.setCity(this.city);
+        clonedCondition.setBusiness(this.business);
+        clonedCondition.setDecorationType(this.decorationType);
+        clonedCondition.setYear(this.year);
+        clonedCondition.setAssessment(this.assessment);
+        clonedCondition.setWeighted(this.weighted);
+        return clonedCondition;
+    }
+
 
 }
