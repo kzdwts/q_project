0e963fb68aa5f75bde16cdf9c610d4eacfe2ce7e
kangyong <kangyong_wts@sina.com>
2024-06-07 14:07:19 +0800

    feat: json字符串转 MD5和Base64编码的组合 作为缓存的key

--- src/main/java/top/kangyong/study02/jsondemo/JsonDemo02.java
+++ src/main/java/top/kangyong/study02/jsondemo/JsonDemo02.java
@@ -2,9 +2,15 @@ package top.kangyong.study02.jsondemo;
 
 import com.alibaba.fastjson.JSON;
 import org.junit.Test;
+import org.springframework.util.DigestUtils;
 import top.kangyong.study02.model.Car;
 import top.kangyong.study02.model.UserDTO;
 import top.kangyong.study02.model.VehicleDTO;
+
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
 
 /**
  * json嵌套 问题
@@ -62,6 +68,37 @@ public class JsonDemo02 {
         System.out.println("c.getWeight() = " + c.getWeight());
         System.out.println("c.getColor() = " + c.getColor());
         System.out.println("c.getDriveMode() = " + c.getDriveMode());
+
+    }
+
+    @Test
+    public void testJsonToBase64MD5() throws NoSuchAlgorithmException {
+        Car car = new Car();
+        car.setBrand("雅迪");
+        car.setColor("小黄鸭");
+        car.setType("两个轮");
+        car.setWeight(100F);
+        car.setPl("1.50L");
+        car.setDriveMode("纯电+人力");
+
+
+        String car1Json = JSON.toJSONString(car);
+
+        // 获取MD5哈希算法实例
+//        MessageDigest md = MessageDigest.getInstance("MD5");
+//        byte[] hashInBytes = md.digest(car1Json.getBytes(StandardCharsets.UTF_8));
+
+        byte[] hashInBytes = DigestUtils.md5Digest(car1Json.getBytes());
+
+        // 将MD5哈希值转换为Base64编码字符串
+        String base64Hash = Base64.getEncoder().encodeToString(hashInBytes);
+        System.out.println("base64Hash = " + base64Hash);
+        // 4dxRQUbxIBZJXJs5wTSsNQ==
+        // 4dxRQUbxIBZJXJs5wTSsNQ==
+        // 4dxRQUbxIBZJXJs5wTSsNQ==
+
+        // p+QWedewpb7zAPwPdnf5oQ==
+        // p+QWedewpb7zAPwPdnf5oQ==
 
     }
 }
